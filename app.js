124c481030d985c1b2ad7e7e3bf7a9caa9180791
Facundo Rubil <facundorubil@MacBook-Air-de-Facundo.local>
2025-06-20 15:52:23 -0300

    añado boton para el calculo

--- app.js
+++ app.js
@@ -1,62 +1,55 @@
-//const elemento = document.getElementById("resultado");
 
-//Guardo elementos del DOM en variables
-const num1_dom = document.getElementById("num1");
-const num2_dom = document.getElementById("num2");
+//guardar boton de calcular 
+const calcular = document.getElementById("btn_calcular");
+// const boton_ocultar = document.getElementById("btn_ocultar");
 
 const resultado_suma = document.getElementById("resultado_suma");
 const resultado_resta = document.getElementById("resultado_resta");
 const resultado_multiplicacion = document.getElementById("resultado_mul");
 const resultado_division = document.getElementById("resultado_div");
-const boton_ocultar = document.getElementById("btn_ocultar");
 const resultado = document.getElementById("resultado");
 
-// Solicitar números al usuario
+calcular.addEventListener("click", function() {
+  const numero1 = Number(document.getElementById("num1").value);
+  const numero2 = Number(document.getElementById("num2").value);
 
-const numero1 = Number(prompt("Ingresa el primer número:"));
-num1_dom.textContent = "valor 1: " + numero1;
-const numero2 = Number(prompt("Ingresea el segundo número:"));
-num2_dom.textContent = "valor 2: " + " " + numero2;
+  // Verificar si los inputs son válidossc
 
-//comienzo a usar funciones para estructurar mejor el codigo...
-function suma() {
-  return numero1 + numero2;
-}
-
-function resta() {
-  return numero1 - numero2;
-}
-
-function multiplicacion() {
-  return numero1 * numero2;
-}
-
-function division() {
-  if (numero2 === 0) {
-    return "Error: División por cero no permitida";
+  if (isNaN(numero1) || isNaN(numero2)) {
+    alert("Ingresa solo numeros");
+    return;
   }
-  return (numero1 / numero2).toFixed(2);
+
+  resultado_suma.textContent = "Suma: " + suma(numero1, numero2);
+  resultado_resta.textContent = "Resta: " + resta(numero1, numero2);
+  resultado_multiplicacion.textContent = "Multiplicación: " + multiplicacion(numero1, numero2);
+  resultado_division.textContent = "División: " + division(numero1, numero2);
+});
+
+// boton_ocultar.addEventListener("click", function() {
+//   resultado.style.display = "none";
+// });
+
+
+// funciones para realizar las operaciones
+
+function suma(a, b) {
+  return a + b;
 }
 
-function mostrar_suma() {
-//   resultado_suma.innerHTML = `<h2>Resultado de la suma:</h2>`;
-  resultado_suma.textContent = suma();
+function resta(a, b) {
+  return a - b;
 }
 
-function mostrar_resta() {
-  resultado_resta.textContent = resta();
+function multiplicacion(a, b) {
+  return a * b;
 }
 
-function mostrar_multiplicacion() {
-  resultado_multiplicacion.textContent = multiplicacion();
-}
+function division(a, b) {
+  if (b === 0) {
+    return "Error: División por cero";
+  }
 
-function mostrar_division() {
-  resultado_division.textContent = division();
+  //retorno la división con 2 decimales.(Recorto con  toFixed)
+  return (a / b).toFixed(2);
 }
-
-// llamar a las funciones
-mostrar_suma();
-mostrar_resta();
-mostrar_multiplicacion();
-mostrar_division();
